b0596657b81b52f3349e7804b1e2b71a56edd9b2
WiktorT <wiktor.tyter@gmail.com>
2018-09-01 14:06:42 +0200

    Added handling course database url

--- MBA.py
+++ MBA.py
@@ -2,14 +2,10 @@ import argparse
 import gtts
 
 languages = gtts.lang.tts_langs()
-language_help = 'Specifies language for which audio will be downloaded. Supported formats: \n'
-
-for code, lang in languages.items():
-    language_help = language_help + code + " : " + lang + '\n'
-
 
 parser = argparse.ArgumentParser(description='This script uploads all missing audio files to the Memrise course')
-parser.add_argument('--language', choices=[*languages], metavar='', help='Specifies language for which audio will be downloaded.  Supported languages: ' + ', '.join([*languages]))
+parser.add_argument('--language', choices=[*languages], metavar='', required=True, help='Specifies language for which audio will be downloaded.  Supported languages: ' + ', '.join([*languages]))
+parser.add_argument('--url', required=True, help='Memrise course database url. For example: https://www.memrise.com/course/1234567/my-memrise-english-course/edit/database/7654321/')
 
 args = parser.parse_args()
 
